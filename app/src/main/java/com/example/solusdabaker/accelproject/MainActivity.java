607eeeafb2c6ee3bbf56241a1a5abb9cc3585ba7
Meghendra Singh <meghsingh89@gmail.com>
2017-12-08 22:27:59 -0500

    Camera logic inserted

--- app/src/main/java/com/example/solusdabaker/accelproject/MainActivity.java
+++ app/src/main/java/com/example/solusdabaker/accelproject/MainActivity.java
@@ -1,56 +1,43 @@
 package com.example.solusdabaker.accelproject;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.os.Handler;
+import android.os.Environment;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Toast;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
-import java.util.concurrent.TimeUnit;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
-    private TextView mTextMessage;
-    private TextView accVals;
     private SensorManager sensorManager;
     private boolean color = false;
-    private View view;
-    private long lastUpdate;
-    private boolean timeDelay;
+    private static long timeDelayMillis = 5000;
+    private static long lastTriggerMillis = 0;
 
+    private static Camera mCamera = null;
+    private CameraPreview mPreview;
+    private boolean safeToTakePicture = false;
 
-//    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-//            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-//
-//        @Override
-//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//            switch (item.getItemId()) {
-//                case R.id.navigation_home:
-//                    mTextMessage.setText(R.string.title_home);
-//                    return true;
-//                case R.id.navigation_dashboard:
-//                    mTextMessage.setText(R.string.title_dashboard);
-//                    return true;
-//                case R.id.navigation_notifications:
-//                    mTextMessage.setText(R.string.title_notifications);
-//                    return true;
-//            }
-//            return false;
-//        }
-//    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,24 +48,21 @@ public class MainActivity extends Activity implements SensorEventListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        accVals = findViewById(R.id.accVals);
-        view = findViewById(R.id.textView);
-        view.setBackgroundColor(Color.GREEN);
-        timeDelay = false;
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        lastUpdate = System.currentTimeMillis();
+        lastTriggerMillis = System.currentTimeMillis();
+        mCamera = Camera.open(0);
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
 
-        //mTextMessage = (TextView) findViewById(R.id.message);
-        //BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             float[] move = sensorEvent.values;
-            accVals.setText(String.valueOf(move));
             // Movement
             try {
                 getAccelerometer(sensorEvent);
@@ -107,27 +91,70 @@ public class MainActivity extends Activity implements SensorEventListener {
         long actualTime = event.timestamp;
         if (accelationSquareRoot <= 0.88) // if accel value is under .88 TAKE PICTURE
         {
-            timeDelay = true;
-            if (actualTime - lastUpdate < 200) {
-                return;
-            }
-            lastUpdate = actualTime;
+            long currentMillis = System.currentTimeMillis();
+            if ((currentMillis - lastTriggerMillis) >= timeDelayMillis) {
+                if (safeToTakePicture) {
+                    mCamera.setDisplayOrientation(90);
+                    mCamera.takePicture(null, null, mPicture);
+                    safeToTakePicture = false;
+                    lastTriggerMillis = currentMillis;
+                } else {
+                    // failed
+                }
 
-            // Place Camera init code here and remove color stuff.
-            if (color) {
-                view.setBackgroundColor(Color.GREEN);
-            } else {
-                view.setBackgroundColor(Color.RED);
-            }
-            color = !color;
-
-            // pause for 1 second
-            if(timeDelay) {
-                TimeUnit.SECONDS.sleep(1);
-                timeDelay = false;
             }
         }
 
+    }
+
+    private PictureCallback mPicture = new PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile();
+            if (pictureFile == null){
+                Log.d("Simple Wear App", "Error creating media file, check storage permissions: " );
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+                safeToTakePicture = true;
+            } catch (FileNotFoundException e) {
+                Log.d("Simple Wear App", "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d("Simple Wear App", "Error accessing file: " + e.getMessage());
+            }
+        }
+    };
+
+    /** Create a File for saving the image*/
+    private static File getOutputMediaFile(){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "AccelApp");
+
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+        // Create the storage directory if it does not exist
+
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("Simple Wear App", "failed to create directory");
+                return null;
+            }
+        }
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp+".jpg");
+        return mediaFile;
     }
 
     @Override
@@ -138,5 +165,69 @@ public class MainActivity extends Activity implements SensorEventListener {
         sensorManager.registerListener(this,
                 sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
                 SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    /** The Camera preview class */
+    public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+        private SurfaceHolder mHolder;
+        private Camera mCamera;
+
+        public CameraPreview(Context context, Camera camera) {
+            super(context);
+            mCamera = camera;
+
+            // Install a SurfaceHolder.Callback so we get notified when the
+            // underlying surface is created and destroyed.
+            mHolder = getHolder();
+            mHolder.addCallback(this);
+            // deprecated setting, but required on Android versions prior to 3.0
+            mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        }
+
+        public void surfaceCreated(SurfaceHolder holder) {
+            // The Surface has been created, now tell the camera where to draw the preview.
+            try {
+                mCamera.setPreviewDisplay(holder);
+                mCamera.setDisplayOrientation(90);
+                mCamera.startPreview();
+                safeToTakePicture = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            // empty. Take care of releasing the Camera preview in your activity.
+        }
+
+        public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+            // If your preview can change or rotate, take care of those events here.
+            // Make sure to stop the preview before resizing or reformatting it.
+
+            if (mHolder.getSurface() == null){
+                // preview surface does not exist
+                return;
+            }
+
+            // stop preview before making changes
+            try {
+                mCamera.stopPreview();
+            } catch (Exception e){
+                // ignore: tried to stop a non-existent preview
+            }
+
+            // set preview size and make any resize, rotate or
+            // reformatting changes here
+
+            // start preview with new settings
+            try {
+                mCamera.setPreviewDisplay(mHolder);
+                mCamera.startPreview();
+                safeToTakePicture = true;
+
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
